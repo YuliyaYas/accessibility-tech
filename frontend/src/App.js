1408b6ccf2bb4f521734d1def65242e02091455a
Yuliya Yasenetska <yuliya@sourcepoint.com>
2021-06-30 21:03:01 -0700

    b/f runs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,16 @@
+import {useEffect, useState} from "react";
+
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
   return (
-    <div>hi</div>
+    <div>{!data ? "Loading..." : data}</div>
   );
 }
 
