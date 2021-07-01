16fd226be751be44eb4c3efd6744b6718b845f31
Yuliya Yasenetska <yuliya@sourcepoint.com>
2021-06-30 21:16:19 -0700

    website access

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
+const cheerio = require('cheerio');
+const got = require('got');
 
-const PORT = process.env.PORT || 3001;
-
+const PORT = 3001;
 const app = express();
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
-  
+
+got("https://yuliyasart.com/").then(response => {
+  const $ = cheerio.load(response.body);
+  console.log($('title')[0].children[0].data)
+}).catch(err => {
+  console.log(err);
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
