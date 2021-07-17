fa64f9fe548e542912d617360e69dc669ef991e7
Yuliya Yasenetska <yuliya@sourcepoint.com>
2021-07-17 14:18:22 -0700

    id rules test

--- backend/tests/idRules.test.js
+++ backend/tests/idRules.test.js
@@ -9,21 +9,19 @@ describe('id rules for Sample page 3', function () {
     it("some ids have duplicates", () => {
         expect(hasDuplicateOrEmptyId($)).toEqual(
             expect.arrayContaining([
-                expect.objectContaining({"category": "id_rule", "description": "Your elements have a duplicate id #logo", "isPassed": false, "name": "Id duplicates and/or empty", "selector": null, "tag": "id", "tip": "Id should be a unique value. Please rename the id attribute to a unique id"})
+                expect.objectContaining({ "category": "id_rule", "description": "Your elements have a duplicate id #logo", "isPassed": false, "name": "Id duplicates and/or empty", "selector": null, "tag": "id", "tip": "Id should be a unique value. Please rename the id attribute to a unique id" })
             ])
-          )
+        )
     });
 
     it("some ids have no value", () => {
         expect(hasDuplicateOrEmptyId($)).toEqual(
             expect.arrayContaining([
-                expect.objectContaining({description: `Your elements have a duplicate id #logo`})
+                expect.objectContaining({ description: `Your elements have a duplicate id #logo` })
             ])
-          )
+        )
     });
 });
-
-
 
 describe('id rules for Sample page 1', function () {
     const $ = cheerio.load(samplePage1.response);
