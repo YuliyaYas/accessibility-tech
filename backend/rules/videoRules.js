de329f9a86c213f67548bcdcbdff5e238d0c3a56
Yuliya Yasenetska <yuliya@sourcepoint.com>
2021-07-28 21:15:32 -0700

    video rules test

--- backend/rules/videoRules.js
+++ backend/rules/videoRules.js
@@ -3,51 +3,37 @@ import { getSelector } from '../utils/utils.js';
 export const hasTrackTagInVideo = ($) => {
     const videoTags = $('video');
     let videoTagsCheck = [];
-    // videoTags.map((_, video) => {
-    //     if (video && video.children) {
-    //         const selector = getSelector(video);
-    //         const track = video.children.filter( (ch) =>  ch.name === 'track')
-    //         if (!track.length){
-    //             videoTagsCheck.push({
-    //                 name: 'video elements missing track',
-    //                 selector,
-    //                 isPassed: false,
-    //                 category: 'video_rule',
-    //                 description: `<video> tag ${selector ? 'with selector "' + selector + '" ' : ''}has no <track> element with [kind="captions"]`,
-    //                 tip: 'Add track tag with [kind="captions"] to the <video> element.',
-    //                 tag: 'video',
-    //                 html: $.html(video)
-    //             });
-    //         } else {
-    //             track.map(t => {
-    //                 if(t.attribs && t.attribs.kind === 'captions'){
-    //                     videoTagsCheck.push({
-    //                         name: 'video elements missing track',
-    //                         selector,
-    //                         isPassed: true,
-    //                         category: 'video_rule',
-    //                         description: `<video> tag ${selector ? 'with selector "' + selector + '" ' : ''}has <track> element with [kind="captions"]`,
-    //                         tip: null,
-    //                         tag: 'video',
-    //                         html: $.html(video)
-    //                     });
-    //                 } else {
-    //                     videoTagsCheck.push({
-    //                         name: 'video elements missing track',
-    //                         selector,
-    //                         isPassed: false,
-    //                         category: 'video_rule',
-    //                         description: `<video> tag ${selector ? 'with selector "' + selector + '" ' : ''}has <track> element but missing [kind="captions"]`,
-    //                         tip: null,
-    //                         tag: 'video',
-    //                         html: $.html(video)
-    //                     });
-    //                 }
-    //             })
-                
-    //         }
-    //     }
-    // })
-    // console.log(videoTagsCheck)
+    videoTags.map((_, video) => {
+        if (video && video.children) {
+            const selector = getSelector(video);
+            const track = video.children.filter((ch) => ch.name === 'track');
+            const hasAnyCaptions = track.filter(t => t.attribs && t.attribs.kind === 'captions').length
+
+            if (!track.length || !hasAnyCaptions) {
+                videoTagsCheck.push({
+                    name: 'video elements missing track',
+                    selector,
+                    isPassed: false,
+                    category: 'video_rule',
+                    description: `<video> tag ${selector ? 'with selector "' + selector + '" ' : ''}has no <track> element with [kind="captions"]`,
+                    tip: 'Add track tag with [kind="captions"] to the <video> element.',
+                    tag: 'video',
+                    html: $.html(video)
+                });
+            } else if (hasAnyCaptions) {
+                videoTagsCheck.push({
+                    name: 'video elements missing track',
+                    selector,
+                    isPassed: true,
+                    category: 'video_rule',
+                    description: `<video> tag ${selector ? 'with selector "' + selector + '" ' : ''}has <track> element with [kind="captions"]`,
+                    tip: null,
+                    tag: 'video',
+                    html: $.html(video)
+                });
+
+            }
+        }
+    })
     return videoTagsCheck;
 }
---
+++ backend/tests/videoRules.test.js
@@ -0,0 +1,22 @@
+import { hasTrackTagInVideo}  from '../rules/videoRules';
+import samplePage1 from './testPages/samplePage1.js';
+import samplePage4 from './testPages/samplePage4.js';
+
+import cheerio from 'cheerio';
+
+describe('Audio tag has track element with [kind="captions"]', function() {
+    it("Shows no errors for sample page 1", () => {
+        const $ = cheerio.load(samplePage1.response);
+        expect(hasTrackTagInVideo($)).toEqual([]);
+    });
+
+    it("Passes when video tag has at least one track tag with correct kind type sample page 4", () => {
+        const $ = cheerio.load(samplePage4.response);
+        expect(hasTrackTagInVideo($)[0].isPassed).toEqual(true);
+    });
+
+    it("Throws error when video tag misses track tag sample page 4", () => {
+        const $ = cheerio.load(samplePage4.response);
+        expect(hasTrackTagInVideo($)[1].isPassed).toEqual(false);
+    });
+});
