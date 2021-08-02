1ea9b26c55dc43eaed633ad310944056416753c1
Yuliya Yasenetska <yuliya@sourcepoint.com>
2021-08-01 20:13:29 -0700

    video test kind descriptions

--- backend/tests/videoRules.test.js
+++ backend/tests/videoRules.test.js
@@ -1,10 +1,10 @@
-import { hasTrackTagWithCaptionsInVideo}  from '../rules/videoRules';
+import { hasTrackTagWithCaptionsInVideo, hasTrackTagWithDescriptionInVideo }  from '../rules/videoRules';
 import samplePage1 from './testPages/samplePage1.js';
 import samplePage4 from './testPages/samplePage4.js';
 
 import cheerio from 'cheerio';
 
-describe('Audio tag has track element with [kind="captions"]', function() {
+describe('Video tag has track element with [kind="captions"]', function() {
     it("Shows no errors for sample page 1", () => {
         const $ = cheerio.load(samplePage1.response);
         expect(hasTrackTagWithCaptionsInVideo($)).toEqual([]);
@@ -18,5 +18,22 @@ describe('Audio tag has track element with [kind="captions"]', function() {
     it("Throws error when video tag misses track tag sample page 4", () => {
         const $ = cheerio.load(samplePage4.response);
         expect(hasTrackTagWithCaptionsInVideo($)[1].isPassed).toEqual(false);
+    });
+});
+
+describe('Video tag has track element with [kind="descriptions"]', function() {
+    it("Shows no errors for sample page 1", () => {
+        const $ = cheerio.load(samplePage1.response);
+        expect(hasTrackTagWithDescriptionInVideo($)).toEqual([]);
+    });
+
+    it("Passes when video tag has at least one track tag with correct with kind=[descriptions] sample page 4", () => {
+        const $ = cheerio.load(samplePage4.response);
+        expect(hasTrackTagWithDescriptionInVideo($)[1].isPassed).toEqual(true);
+    });
+
+    it("Throws error when video tag misses track tag with kind=[descriptions] sample page 4", () => {
+        const $ = cheerio.load(samplePage4.response);
+        expect(hasTrackTagWithDescriptionInVideo($)[0].isPassed).toEqual(false);
     });
 });
