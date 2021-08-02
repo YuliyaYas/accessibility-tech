bc1993b0ee96208f682c55f2fbcdaf3f8e67732d
Yuliya Yasenetska <yuliya@sourcepoint.com>
2021-08-01 20:10:39 -0700

    video rule descriptions

--- backend/rules/videoRules.js
+++ backend/rules/videoRules.js
@@ -1,6 +1,6 @@
 import { getSelector } from '../utils/utils.js';
 
-export const hasTrackTagInVideo = ($) => {
+export const hasTrackTagWithCaptionsInVideo = ($) => {
     const videoTags = $('video');
     let videoTagsCheck = [];
     videoTags.map((_, video) => {
@@ -27,6 +27,44 @@ export const hasTrackTagInVideo = ($) => {
                     isPassed: true,
                     category: 'video_rule',
                     description: `<video> tag ${selector ? 'with selector "' + selector + '" ' : ''}has <track> element with [kind="captions"]`,
+                    tip: null,
+                    tag: 'video',
+                    html: $.html(video)
+                });
+
+            }
+        }
+    })
+    return videoTagsCheck;
+}
+
+export const hasTrackTagWithDescriptionInVideo = ($) => {
+    const videoTags = $('video');
+    let videoTagsCheck = [];
+    videoTags.map((_, video) => {
+        if (video && video.children) {
+            const selector = getSelector(video);
+            const track = video.children.filter((ch) => ch.name === 'track');
+            const hasAnyDescription= track.filter(t => t.attribs && t.attribs.kind === 'descriptions').length
+
+            if (!track.length || !hasAnyDescription) {
+                videoTagsCheck.push({
+                    name: 'video elements missing track',
+                    selector,
+                    isPassed: false,
+                    category: 'video_rule',
+                    description: `<video> tag ${selector ? 'with selector "' + selector + '" ' : ''}has no <track> element with [kind="descriptions"]`,
+                    tip: 'Add track tag with [kind="descriptions"] to the <video> element.',
+                    tag: 'video',
+                    html: $.html(video)
+                });
+            } else if (hasAnyDescription) {
+                videoTagsCheck.push({
+                    name: 'video elements missing track',
+                    selector,
+                    isPassed: true,
+                    category: 'video_rule',
+                    description: `<video> tag ${selector ? 'with selector "' + selector + '" ' : ''}has <track> element with [kind="descriptions"]`,
                     tip: null,
                     tag: 'video',
                     html: $.html(video)
--- backend/tests/videoRules.test.js
+++ backend/tests/videoRules.test.js
@@ -1,4 +1,4 @@
-import { hasTrackTagInVideo}  from '../rules/videoRules';
+import { hasTrackTagWithCaptionsInVideo}  from '../rules/videoRules';
 import samplePage1 from './testPages/samplePage1.js';
 import samplePage4 from './testPages/samplePage4.js';
 
@@ -7,16 +7,16 @@ import cheerio from 'cheerio';
 describe('Audio tag has track element with [kind="captions"]', function() {
     it("Shows no errors for sample page 1", () => {
         const $ = cheerio.load(samplePage1.response);
-        expect(hasTrackTagInVideo($)).toEqual([]);
+        expect(hasTrackTagWithCaptionsInVideo($)).toEqual([]);
     });
 
     it("Passes when video tag has at least one track tag with correct kind type sample page 4", () => {
         const $ = cheerio.load(samplePage4.response);
-        expect(hasTrackTagInVideo($)[0].isPassed).toEqual(true);
+        expect(hasTrackTagWithCaptionsInVideo($)[0].isPassed).toEqual(true);
     });
 
     it("Throws error when video tag misses track tag sample page 4", () => {
         const $ = cheerio.load(samplePage4.response);
-        expect(hasTrackTagInVideo($)[1].isPassed).toEqual(false);
+        expect(hasTrackTagWithCaptionsInVideo($)[1].isPassed).toEqual(false);
     });
 });
