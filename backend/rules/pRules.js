d5a606acad35932b0f26752dc8a6309e2a5f88ca
Yuliya Yasenetska <yuliya@sourcepoint.com>
2021-08-02 20:51:26 -0700

    p tag fix

--- backend/rules/pRules.js
+++ backend/rules/pRules.js
@@ -13,12 +13,55 @@ export const hasTabIndexInPTag = ($) => {
                 category: 'p_rule',
                 description: `<p> tag ${selector ? 'with selector "'+selector+ '" ' : ''}has a tabindex attribute`,
                 tip: 'Please remove tabindex attribute from the <p> tag. Text is not meant to be navigated with a tab key',
-                tag: 'html',
+                tag: 'p',
                 html: $.html(p)
             })
         }
-    })
+    });
 
-    //TODO: add rule for p tags with children. P tag should have no children
-    return pTagsCheck
+    if (pTags.length && !pTagsCheck.length){
+        return [{
+            name: 'P attribute',
+            selector,
+            isPassed: true,
+            category: 'p_rule',
+            description: `<p> tags has no tabindex attribute`,
+            tip: null,
+            tag: 'p',
+            html: null
+        }]
+    } else return pTagsCheck;
+}
+
+export const hasChildrenInPTag = ($) => {
+    const pTags = $('p');
+    let pTagsCheck = [];
+    pTags.map((_, p) => {
+        if (p.attribs && p.attribs.tabindex) {
+            const selector = getSelector(p);
+            pTagsCheck.push({
+                name: 'P attribute',
+                selector,
+                isPassed: false,
+                category: 'p_rule',
+                description: `<p> tag ${selector ? 'with selector "'+selector+ '" ' : ''}has a tabindex attribute`,
+                tip: 'Please remove tabindex attribute from the <p> tag. Text is not meant to be navigated with a tab key',
+                tag: 'p',
+                html: $.html(p)
+            })
+        }
+    });
+
+    if (pTags.length && !pTagsCheck.length){
+        return [{
+            name: 'P attribute',
+            selector,
+            isPassed: true,
+            category: 'p_rule',
+            description: `<p> tags has no tabindex attribute`,
+            tip: null,
+            tag: 'p',
+            html: null
+        }]
+    } else return pTagsCheck;
 }
