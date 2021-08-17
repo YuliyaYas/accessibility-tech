62e9d01bc65ef991959a239d033c2a1445525d31
Yuliya Yasenetska <yuliya@sourcepoint.com>
2021-08-16 19:19:27 -0700

    cors

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,13 +1,14 @@
 import { getPage }  from '../controller/scraper.js';
 import express from 'express'
+import cors from 'cors';
 
 const PORT = 3001;
 const app = express();
-
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+  res.json({ message: "Hello from server!!" });
 });
 
 app.post('/page-scan', (req, res) => {
